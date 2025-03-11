7bfa8ae46a0c135ec10e2ce6d8c6d10e352f682f
jeffyololo <jeffyololo@gmail.com>
2025-03-11 21:46:03 +0100

    fix

--- components/toilet_board/toilet_board.h
+++ components/toilet_board/toilet_board.h
@@ -2,7 +2,6 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/text/text.h"
-#include "esphome/components/text/text_traits.h"
 #include "esphome/core/hal.h"
 
 namespace esphome {
@@ -12,26 +11,18 @@ class ToiletBoard: public PollingComponent, public text::Text
 {
 public:
     void set_data_pin(InternalGPIOPin *data_pin) { data_pin_ = data_pin; }
-    void set_city(const char* city) { city_ = city; }
-    void set_text(text::Text* text) { text_ = text; }
-
-    void set_name(const char *) {}
-    void set_object_id(const char *) {}
-    void set_disabled_by_default(bool) {}
+    void set_city(const char *city) { city_ = city; }
 
     void setup() override {}
     void dump_config() override;
     void loop() override {}
     void update() override {}
 
-    text::TextTraits traits;
-
 protected:
     void control(const std::string &value) override {}
 
     InternalGPIOPin *data_pin_;
-    const char* city_;
-    text::Text* text_;
+    const char *city_;
 };
 
 }
