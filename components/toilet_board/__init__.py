7a804a0c64142a07238c0ec8b257d1d48c44b16a
jeffyololo <jeffyololo@gmail.com>
2025-03-11 22:18:15 +0100

    debug

--- components/toilet_board/__init__.py
+++ components/toilet_board/__init__.py
@@ -33,6 +33,7 @@ CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(text.TEXT_SCHEMA.extend({
 ).extend(cv.polling_component_schema("1s")))
 
 async def to_code(config):
+    print(config[CONF_NAME])
     # var = cg.new_Pvariable(config[CONF_ID])
     var = await text.new_text(config)
     await cg.register_component(var, config)
