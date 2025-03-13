f4d2d2a75e33dc7bc57136eb822485cb14a4d3fa
jeffyololo <jeffyololo@gmail.com>
2025-03-13 22:16:18 +0100

    trying to use something

--- components/toilet_board/toilet_board.cpp
+++ components/toilet_board/toilet_board.cpp
@@ -8,6 +8,28 @@ namespace Board {
 
 static constexpr auto TAG = "tb";
 
+typedef ColumnMajorAlternatingLayout MyPanelLayout;
+// typedef ColumnMajorLayout MyPanelLayout;
+// typedef RowMajorAlternatingLayout MyPanelLayout;
+// typedef RowMajorLayout MyPanelLayout;
+
+// make sure to set these panel values to the sizes of yours
+const uint8_t PanelWidth = 16; // 16 pixel x 16 pixel matrix of leds
+const uint8_t PanelHeight = 16;
+const uint8_t TileWidth = 4; // laid out in 4 panels x 1 panel mosaic
+const uint8_t TileHeight = 1;
+
+const uint16_t PixelCount = PanelWidth * PanelHeight * TileWidth * TileHeight;
+const uint8_t PixelPin = 15;
+
+NeoMosaic<MyPanelLayout> mosaic(
+    PanelWidth,
+    PanelHeight,
+    TileWidth,
+    TileHeight);
+
+NeoPixelBus<NeoGrbFeature, NeoWs2812xMethod> strip(PixelCount, PixelPin);
+
 void ToiletBoard::dump_config()
 {
     ESP_LOGCONFIG(TAG, "toilet_board:");
